aa9979ac111f2b9dccd10aca6faf6c181ef2c6e6
Lucas-Hudson <lucashudson.contact@gmail.com>
2019-08-08 17:17:38 +0200

    Create User

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,24 @@ class SessionsController < ApplicationController
   end
 
   def create
+    # LOGIN
+      #   puts "#" * 100
+      # # cherche s'il existe un utilisateur en base avec l’e-mail
+      # user = User.find_by(:email => params[:email])
+      #
+      # # on vérifie si l'utilisateur existe bien ET si on arrive à l'authentifier (méthode bcrypt) avec le mot de passe
+      #   if user && user.authenticate(params[:password])
+      #     session[:user_id] = user.id
+      #     # redirige où tu veux, avec un flash ou pas
+      #     redirect_to root_path
+      #
+      #   else
+      #     flash.now[:danger] = 'Invalid email/password combination'
+      #     render 'new'
+      #   end
   end
 
   def destroy
   end
-  
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,7 @@
-require 'bcrypt'
-
 class UsersController < ApplicationController
   def index
   end
+
   def show
     @user = User.find(params[:id])
   end
@@ -12,13 +11,43 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new
-   end
+    u = User.new
+    u.email = params[:email]
+    u.password = params[:password]
+    u.city_id = 20
+    u.save
+    # a = City.find_by(:name => params[:city])
+    # puts "@" * 100
+    # puts a.blank?
+    # puts "@" * 100
+
+    # if a.blank? == true
+    #   puts "@" * 100
+    #   puts "C'est BLANK"
+    #   puts "@" * 100
+    #   City.create(:name => params[:city])
+    #   a2 = City.find_by(:name => params[:city]).id
+    #   u.city_id = a2
+    # else
+    #   puts "@" * 100
+    #   puts "C'est PAS BLANK"
+    #   puts "@" * 100
+    #   a = City.find_by(:name => params[:city]).id
+    #   puts "#" * 100
+    #   puts a
+    #   puts "#" * 100
+    #   u.city_id = a
+    #   u.save
+    # end
   end
 
   def edit
   end
+
   def update
   end
+
   def destroy
   end
+
+end
